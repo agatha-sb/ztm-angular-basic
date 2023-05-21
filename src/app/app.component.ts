c30c0a93530692d359d8a3cc0cfd6f107a227c9a
agatha-sb <swagatabose27@gmail.com>
2023-05-21 22:51:40 +0530

    ztm ng basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,42 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // template: `<p>Hello world!</p>`,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // styles: [
+  //   `
+  //     p {
+  //       color: orange;
+  //     }
+  //   `,
+  // ],
 })
 export class AppComponent {
-  title = 'basic';
+  name = 'swagata bose';
+  imageURL = 'https://picsum.photos/id/237/500/500';
+  images = [
+    'https://picsum.photos/id/237/500/500',
+    'https://picsum.photos/id/237/500/500',
+    'https://picsum.photos/id/237/500/500',
+  ];
+  currentDate = new Date();
+  cost = 2000;
+  temperature = 25.3;
+  pizza = {
+    toppings: ['Bacon', 'Pepperoni'],
+    size: 'Large',
+  };
+  blueClass = false;
+  fontSize = 16;
+
+  getName() {
+    return this.name;
+  }
+  changeImage(e: KeyboardEvent) {
+    this.imageURL = (e.target as HTMLInputElement).value;
+  }
+  logImg(event: string) {
+    console.log(event);
+  }
 }
